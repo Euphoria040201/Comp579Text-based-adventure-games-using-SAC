a282a579d5dd857c3cc952cc16cdf3a1c04f6f46
Qingchen Hu <qingchen.hu@mail.mcgill.ca>
2025-04-03 13:48:14 -0400

    cluster script

---
+++ src/job.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#SBATCH --partition=long
+#SBATCH --mem=50GB
+#SBATCH --time=60:00:00
+#SBATCH --output=sbatch_out/textgame1.%A.out
+#SBATCH --job-name=textgame1
+
+source myenv/bin/activate
+module load miniconda
+conda activate /home/mila/q/qingchen.hu/textgame_579/src/myenv
+
+python train.py --output_dir 'output' \
+    --rom_path 'z-machine-games-master/jericho-game-suite/zork3.z5' \
+    --spm_path 'unigram_8k.model' --wandb=1 --wandb_project="579_textgame"\
+    --max_steps=5000
+    
